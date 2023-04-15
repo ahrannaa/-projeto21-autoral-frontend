d9a107ff536e5d7fc3a7e5c6b2019746abd3d743
Ahranna Araujo <ahrannaaraujo@ahrannas-air.lan>
2023-04-15 17:27:18 +0100

    layout service page

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,8 +1,57 @@
 import styled from "styled-components";
+import TopBarPage from "./TopBarPage";
 
 export default function ServicePage(){
   return (
-
+    <>
+    <TopBarPage></TopBarPage>
+     <Container>
+      <ServiceWrapper>
+        <Service>
+          <p>Coloração cabelos longos</p>
+          <p>Valor: R$ 150,00</p>
+          <button>agendar</button>
+        </Service>
+      </ServiceWrapper>
+    </Container>
+    </> 
   )
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content:center;
+  margin-top: 20px;
+`;
+const ServiceWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const Service = styled.div`
+  width: 250px;
+  height: 130px;
+  background-color: #8a5755;
+  border-radius:5px;
+  margin: 20px;
+
+  p {
+    font-size: 20px;
+    color: #ffffff;
+    justify-content:center;
+    text-align: center;
+  }
+
+  button {
+   width: 100px;
+   height: 20px;
+   border: solid 1px #8a5755;
+   border-radius: 5px;
+   color:#8a5755;
+   background-color:white;
+   margin: 0 80px;
+   cursor: pointer;
+   margin-right: 10px;
+  }
+`
---
+++ src/pages/TopBarPage.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export default function TopBarPage(){
+ 
+  return (
+   <TopBar>
+    <LinkWrapper>
+      <Link href="#">Charme Feminino</Link>
+    </LinkWrapper>
+    </TopBar>
+  )
+};
+
+const TopBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #8a5755;
+  height: 80px;
+  padding: 0 20px;
+`;
+const LinkWrapper = styled.div`
+  display: flex;
+`;
+const Link = styled.a`
+  color: white;
+  text-decoration: none;
+  margin-left: 20px;
+  font-size: 60px;
+`;
